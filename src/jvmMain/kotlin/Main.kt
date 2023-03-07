e4c15c9b8bf60d77530ff0bc18fb94d9da8c619f
BuildTools <unconfigured@null.spigotmc.org>
2023-03-07 20:50:45 +0100

    Added close and minimize buttons

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,36 +1,44 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import components.TopBar
 import util.maximizeUndecorated
+import kotlin.system.exitProcess
+
+class MainWindow {
+
+    companion object {
+        lateinit var state: WindowState
+
+        fun close() {
+            exitProcess(0)
+        }
+    }
+
+}
+
+fun setState(windowState: WindowState): WindowState {
+    MainWindow.state = windowState
+    return windowState
+}
 
 @Composable
 @Preview
 fun App() {
 
-    MaterialTheme {
-
-        TopBar()
-
-    }
+    TopBar()
 
 }
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        state = rememberWindowState().maximizeUndecorated(),
         undecorated = true,
         resizable = false,
+        state = setState(rememberWindowState().maximizeUndecorated()),
         title = "Cobblestone - Mod Maker",
     ) {
         App()
